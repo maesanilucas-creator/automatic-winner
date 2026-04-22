4686c8920495e6a6b20eee19f10312491d1a8044
maesanilucas-creator <maesani.lucas@gmail.com>
2026-04-22 11:05:10 +0200

    Update server_main.py

--- server_main.py
+++ server_main.py
@@ -1,8 +1,18 @@
 """Programme principal qui créer une partie complète avec plusieurs joueurs
-
 """
+import server
+import Logic
+import game_state_empty
 
 1. créer GameServer
 2. écouter connexions (stocke les joueurs)
 3. créer partie quand joueurs prêts
 4. boucle infinie
+
+def Initialisation_Server():
+  server.Host().start()
+
+def Initialisation_Client():
+  server.Client.start()
+  game_state = server.Host.get_state
+  Logic.Partie(game_state)
